e363576c2ea562bead491192bf3957d17f854785
ktjack2009 <736738213@qq.com>
2018-11-16 12:01:50 +0800

    命令行获取参数

--- codes/01.py
+++ codes/01.py
@@ -4,7 +4,7 @@ import tornado.options
 import tornado.httpserver
 
 from tornado.web import RequestHandler
-from tornado.options import define, options
+from tornado.options import define, options, parse_command_line
 
 define("port", type=int, default=8888, help="服务器端口")
 
@@ -13,16 +13,21 @@ define("port", type=int, default=8888, help="服务器端口")
 class IndexHandler(RequestHandler):
     "主页处理类"
 
-    def get(self):
-        "get请求方式"
+    # get请求方式
+    def get(self, *args, **kwargs):
+        # 响应信息
         self.write('hello world')
 
 
 if __name__ == '__main__':
+    parse_command_line()    # 从命令行获取参数
+    print(options.port)
+    '''
     app = tornado.web.Application([
         ('/', IndexHandler),
     ])
-    http_server = tornado.httpserver.HTTPServer(app)
+    http_server = tornado.httpserver.HTTPServer(app)  # 创建服务器
     http_server.bind(options.port)  # 服务器绑定到指定端口
-    http_server.start(num_processes=0)  # 开启多进程，num_processes默认为1，None 或 <=0则根据cpu
+    http_server.start(num_processes=1)  # 开启多进程，num_processes默认为1，None 或 <=0则根据cpu
     tornado.ioloop.IOLoop.current().start()  # 不断的循环询问epoll
+    '''
--- tornado_笔记.py
+++ tornado_笔记.py
@@ -1,4 +1,4 @@
-引言='''
+引言 = '''
 以Django为代表的python web应用部署时采用wsgi协议与服务器对接（被服务器托管），
 而这类服务器通常都是基于多线程的，也就是说每一个网络请求服务器都会有一个对应的线
 程来用web应用（如Django）进行处理。
@@ -9,24 +9,57 @@
 	对于这两种情况，通常基于多线程的服务器很难应对。
 
 C10K问题：并发10000个连接，对于单台服务器而言，根本无法承担，而采用多台服务器分布式又意味着高昂的成本。
+
+Tornado使用了单线程事件循环，所以意味着所有的应用代码必须是异步非阻塞的
+
+异步（协程）的写法：
+from tornado.httpclient import AsyncHTTPClient
+
+async def asynchronous_fetch(url):
+    http_client = AsyncHTTPClient()
+    response = await http_client.fetch(url)
+    return response.body
+
+老版本的写法：
+from tornado.httpclient import AsyncHTTPClient
+from tornado import gen
+
+@gen.coroutine
+def async_fetch_gen(url):
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(url)
+    raise gen.Return(response.body)
+
+协程起到的作用类似于：
+from tornado.concurrent import Future
+def async_fetch_manual(url):
+    http_client = AsyncHTTPClient()
+    my_future = Future()
+    fetch_future = http_client.fetch(url)
+    def on_fetch(f):
+        my_future.set_result(f.result().body)
+    fetch_future.add_done_callback(on_fetch)
+    return my_future
 '''
 
-Title_1='''
+Title_1 = '''
 Tornado特点：
 	1. 轻量级web框架，拥有异步非阻塞IO的处理方式
 	2. 出色的抗负载能力
 
+tornado.web:
+    > 基础web框架模块
 tornado.ioloop:
-    tornado的核心io循环模块，封装了Linux的epoll和BSD的kqueue，高性能的基石
+    > tornado的核心io循环模块，封装了Linux的epoll和BSD的kqueue，高性能的基石
 tornado.httpserver:
-    tornado的HTTP服务器实现
+    > tornado的HTTP服务器实现
 app.listen:
-    这个方法只能用在单进程中
-Application：web应用框架的核心，是与服务器对接的接口，里面保存了路由信息表，其初始化第一个参数就是一个路由
-    信息映射元祖的列表
+    > 这个方法只能用在单进程中
+Application：
+    > web应用框架的核心，是与服务器对接的接口，里面保存了路由信息表，其初始化第一个参数就是一个路由信息映射元祖的列表
 RequestHandler:
-    封装了对应一个请求的所有信息和方法，write（响应信息）就是写响应的一个方法；对应每一种http请求方式（get、post
-    等），把对应的处理逻辑写进同名的成员方法中
+    > 封装了对应一个请求的所有信息和方法，write（响应信息）就是写响应的一个方法；对应每一种http请求方式
+        （get、post等），把对应的处理逻辑写进同名的成员方法中
     
 关于多进程：
     > 虽然tornado提供了一次开启多个进程的方法，但由于每个子进程都会从父进程中复制一份IOLoop实例，那么会影响到每一个子进程，
